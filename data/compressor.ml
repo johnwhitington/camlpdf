ab48611f6fd920509c4b1288d833d768123a1f71
John Whitington <john@coherentgraphics.co.uk>
2021-09-30 17:22:06 +0100

    args

--- data/compressor.ml
+++ data/compressor.ml
@@ -1,1 +1,12 @@
-(* compress source file data *)
+(* compressor <infile> <outfile> [<compress>] substitutes data files specified
+   by __DATA:<filename>\n into the <infile> template, writing to <outfile>. The
+   data is printed in way which meets OCaml's syntax. It is optionally
+   compressed by zlib. *)
+
+let go infile outfile compress = ()
+
+let () =
+  match Sys.argv with
+  | [|_; infile; outfile|] -> go infile outfile false
+  | [|_; infile; outfile; "compress"|] -> go infile outfile true
+  | _ -> Printf.eprintf "compressor: unknown command line\n"
