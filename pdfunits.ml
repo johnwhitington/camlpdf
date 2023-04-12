172f14f3e5194fce1cd81b4338157b838b97c397
John Whitington <john@coherentgraphics.co.uk>
2023-04-12 16:09:19 +0100

    more

--- pdfunits.ml
+++ pdfunits.ml
@@ -2,6 +2,6 @@ type t = PdfPoint | Inch | Centimetre | Millimetre
 
 let points x = function
   | PdfPoint -> x
-  | Inch -> x /. 72.
-  | Centimetre -> x /. 72. *. 2.54
-  | Millimetre -> x /. 72. *. 2.54 *. 10.
+  | Inch -> x *. 72.
+  | Centimetre -> x *. 28.3465
+  | Millimetre -> x *. 2.83465
