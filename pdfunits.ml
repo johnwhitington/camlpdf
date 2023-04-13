96541882174231cd00c1afafa0a878b78c8343f8
John Whitington <john@coherentgraphics.co.uk>
2023-04-13 21:55:59 +0100

    more

--- pdfunits.ml
+++ pdfunits.ml
@@ -5,3 +5,18 @@ let points x = function
   | Inch -> x *. 72.
   | Centimetre -> x *. 28.3465
   | Millimetre -> x *. 2.83465
+
+let inches x = function
+  | Inch -> x
+  | PdfPoint -> x /. 72.
+  | Centimetre -> x /. 2.54
+  | Millimetre -> x /. 25.4
+
+let centimetres x = function
+  | Centimetre -> x
+  | Inch -> x *. 2.54
+  | Millimetre -> x /. 10.
+  | PdfPoint -> x /. 28.3456
+
+let millimetres x u =
+  centimetres x u *. 10.
--- pdfunits.mli
+++ pdfunits.mli
@@ -5,3 +5,12 @@ type t = PdfPoint | Inch | Centimetre | Millimetre
 
 (** Convert a measurement to points *)
 val points : float -> t -> float
+
+(** Convert a measurement to inches *)
+val inches : float -> t -> float
+
+(** Convert a measurement to centimetres *)
+val centimetres : float -> t -> float
+
+(** Convert a measurement to millimetres *)
+val millimetres : float -> t -> float
