a1b03c87680567630e82d50d6d25beb7f1f6644d
John Whitington <john@coherentgraphics.co.uk>
2024-01-15 18:24:40 +0000

    Remove very old, unused, Pdfimage code

--- pdfimage.mli
+++ pdfimage.mli
@@ -19,27 +19,6 @@ bytes RGBRGB etc. In all instances, if JPEG or JPEG2000 or JBIG2 is the compress
 val get_image_24bpp :
   Pdf.t -> Pdf.pdfobject -> Pdf.pdfobject -> t
 
-(** Similarly, but if it's to be Raw, use the smallest pixel layout required to
-represent the data. Not implemented yet. *)
-val get_image :
-  Pdf.t -> Pdf.pdfobject -> Pdf.pdfobject -> t
-
-(** Get the uncompressed image contents in 24bpp, decoding JPEG, JPEG2000 and JBIG2 as
-required. Not implemented yet. *)
-val get_image_raw_24bpp :
-  Pdf.t -> Pdf.pdfobject -> Pdf.pdfobject -> t
-
-(** Get the uncompressed image contents, decoding JPEG, JPEG2000 and JBIG2 as
-required. Not implemented yet. *)
-val get_image_raw :
-  Pdf.t -> Pdf.pdfobject -> Pdf.pdfobject -> t
-
-(** Return a function which, when given an x and y coordinate, returns the pixel
-byte values prior to any decoding, i.e in the raw input image data before /Decode,
-/Index lookups and so on. Returns array of components as bytes. *)
-val get_image_unprocessed_pixel : Pdf.t -> Pdf.pdfobject -> Pdf.pdfobject
--> (int -> int array) option
-
 (** Given a PDF, an image and a /Resources dictionary, return the colourspace
 of the image *)
 val colspace : Pdf.t -> Pdf.pdfobject -> Pdf.pdfobject -> Pdfspace.t
