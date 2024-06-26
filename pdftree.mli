bfe49cf2b84d92e9ecc82ad56bdc225e3264e367
John Whitington <john@coherentgraphics.co.uk>
2024-06-26 16:46:12 +0100

    Document (to change) pdftree API

--- pdftree.mli
+++ pdftree.mli
@@ -1,5 +1,13 @@
+(** Name and Number Trees *)
+ 
+(** Subject to API changes. *)
+
+(** Read a name tree as a flat (key, value) list *)
 val read_name_tree : Pdf.t -> Pdf.pdfobject -> (string * Pdf.pdfobject) list
 
+(** Read a number tree as a flat (key, value) list but with the numbers as strings. *)
 val read_number_tree : Pdf.t -> Pdf.pdfobject -> (string * Pdf.pdfobject) list
 
+(** Build a name or number tree (bool: true if number, false otherwise) from
+    flat (key, value) list *)
 val build_name_tree : bool -> Pdf.t -> (string * Pdf.pdfobject) list -> Pdf.pdfobject
