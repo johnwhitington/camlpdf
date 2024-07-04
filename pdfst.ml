a82b29f1b10a598d68109ef604833997f840bb17
John Whitington <john@coherentgraphics.co.uk>
2024-07-04 16:18:56 +0100

    Working on structure tree trimmer

--- pdfst.ml
+++ pdfst.ml
@@ -24,11 +24,58 @@ let trim_structure_tree pdf range =
                end
            | _ -> ())
         pdf;
-      iter (Pdf.removeobj pdf) (setify_large !del);
       (* Any /K referencing these deleted objects is modifed to no longer reference
          it. We don't rely on nulls, but do the actual modification. *)
-      Pdf.objiter
-        (fun n o ->
-           ())
-        pdf;
-      flprint "trim_structure_tree\n"
+      while !del <> [] do
+        iter (Pdf.removeobj pdf) (setify_large !del);
+        del := [];
+        Pdf.objiter
+          (fun n o ->
+             let process objs = None in
+             match Pdf.direct pdf o with
+             | Pdf.Dictionary d ->
+                 begin match List.assoc_opt "/K" d with
+                 | Some (Pdf.Indirect _ | Pdf.Integer _ as obj) ->
+                     (* Could be structelem or marked-content reference dictionary
+                     or object reference dictionary depending on /Type (no /Type =
+                     assume structelem *)
+                     begin match process [obj] with
+                     | None -> () (* no change *)
+                     | Some newlist ->
+                         (* update the value of /K and change the object in place *)
+                         ()
+                     end
+                 | Some (Pdf.Array objs) ->
+                     (* An array of indirects or integers. Empty arrays not allowed. *)
+                     begin match process objs with
+                     | None -> () (* no change *)
+                     | Some [] ->
+                         (* empty not allowed - we must now delete this object?
+                            But what if it chains up? Go until no more change? *)
+                         ()
+                     | Some newlist ->
+                         (* update the value of /K and change the object in place *)
+                         ()
+                     end
+                 | _ -> ()
+                 end
+             | _ -> ())
+          pdf
+        done
+
+
+(* FIXME Are we sure no other objects can have /K or /Pg? Do we need to read
+   the struct tree object list to restrict what we touch? *)
+
+(* FIXME Do we need a more careful approach anyway? What does the verifier /
+   spec have to say on the matter? *)
+
+(* FIXME Is our approach tough enough - should we be cutting structures further
+   up the tree if they don't correspond to anything marked with a /Pg? *)
+
+(* FIXME Eventually, this will be replaced with a proper model which a) parses
+   the whole structure tree, modifies it as a tree and re-writes it *)
+
+(* FIXME Do we need to look inside content streams to find MCIDs and make sure
+   no MCID remaining in the tree points to a now non-extant one? Does the
+   verifier care? *)
