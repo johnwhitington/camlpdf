b6eda3f8bd5374aefb5dd3ddc2be8e48a2a24e58
John Whitington <john@coherentgraphics.co.uk>
2024-07-06 15:38:22 +0100

    Tidy up

--- pdfst.ml
+++ pdfst.ml
@@ -12,7 +12,7 @@ let trim_structure_tree pdf range =
     let objnums = Pdf.page_reference_numbers pdf in
       map (fun x -> List.nth objnums (x - 1)) (setminus (ilist 1 (!endpage pdf)) range)
   in
-    (* Any object with /Pg not in range is deleted. *)
+    (* Calculate initial deletions - any object with /Pg not in range. *)
     let del = ref [] in
       Pdf.objiter 
         (fun n o ->
@@ -25,8 +25,7 @@ let trim_structure_tree pdf range =
                end
            | _ -> ())
         pdf;
-      (* Any /K referencing these deleted objects is modifed to no longer reference
-         it. We don't rely on nulls, but do the actual modification. *)
+      (* Any /K referencing these deleted objects is modifed to no longer reference it. *)
       let replaceobjs = ref [] in
       while !del <> [] do
         (*Printf.printf "Top of loop. %i to remove, %i to replace\n" (length (setify_large !del)) (length (setify_large !replaceobjs));
@@ -61,11 +60,8 @@ let trim_structure_tree pdf range =
                | Some newlist ->
                    (* update the value of /K and change the object in place *)
                    begin match newlist with
-                   | [e] ->
-                       replaceobjs =| (n, Pdf.replace_dict_entry (Pdf.Dictionary d) "/K" e)
-                   | e::es ->
-                       (* Make a new /K array, copying integers and indirects (noting them in replaceobjs) *)
-                       replaceobjs =| (n, Pdf.replace_dict_entry (Pdf.Dictionary d) "/K" (Pdf.Array (e::es)))
+                   | [e] -> replaceobjs =| (n, Pdf.replace_dict_entry (Pdf.Dictionary d) "/K" e)
+                   | e::es -> replaceobjs =| (n, Pdf.replace_dict_entry (Pdf.Dictionary d) "/K" (Pdf.Array (e::es)))
                    | _ -> assert false
                    end
                end
@@ -74,12 +70,8 @@ let trim_structure_tree pdf range =
                | Pdf.Dictionary d ->
                    begin match List.assoc_opt "/K" d with
                    | Some (Pdf.Integer _) -> ()
-                   | Some (Pdf.Indirect i) ->
-                       (*Printf.printf "Process /K indirect %s\n" (Pdfwrite.string_of_pdf (Pdf.Dictionary d));*)
-                       process_indirect d [Pdf.Indirect i]
-                   | Some (Pdf.Array objs) ->
-                       (*Printf.printf "Process /K array %s\n" (Pdfwrite.string_of_pdf (Pdf.Dictionary d));*)
-                       process_indirect d objs
+                   | Some (Pdf.Indirect i) -> process_indirect d [Pdf.Indirect i]
+                   | Some (Pdf.Array objs) -> process_indirect d objs
                    | _ -> ()
                    end
              | _ -> ())
@@ -89,14 +81,8 @@ let trim_structure_tree pdf range =
 (* FIXME Are we sure no other objects can have /K or /Pg? Do we need to read
    the struct tree object list to restrict what we touch? *)
 
-(* FIXME Do we need a more careful approach anyway? What does the verifier /
-   spec have to say on the matter? *)
-
-(* FIXME Is our approach tough enough - should we be cutting structures further
-   up the tree if they don't correspond to anything marked with a /Pg? *)
-
 (* FIXME Eventually, this will be replaced with a proper model which a) parses
-   the whole structure tree, modifies it as a tree and re-writes it *)
+   the whole structure tree, modifies it as a tree and re-writes it? *)
 
 (* FIXME Do we need to look inside content streams to find MCIDs and make sure
    no MCID remaining in the tree points to a now non-extant one? Does the
